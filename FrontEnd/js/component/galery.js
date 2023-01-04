25584a4b31336501d32f318fdd87116a1f9f3465
dh ch <chaalaadhif@gmail.com>
2023-01-04 15:20:38 +0100

    boucle de creation des figures

--- FrontEnd/js/component/galery.js
+++ FrontEnd/js/component/galery.js
@@ -11,9 +11,31 @@ const affiche = async () => {
     .then((res) => res.json());
     return list;
 }
-const galery = (listwork) => {
-    let images = document.querySelector(".galery");
-    listwork.map((elt)=> {
+//creations des 3 elements 
+image.src=list[i].imageUrl;
+titre= list[i].title;
+for (let i = 0; i <= id-1; i++) {
+let figure = document.createElement("figure");
+let image=document.createElement("img");
+let titre=document.createElement("figcaption");
+figure.appendChild(image);
+figure.appendChild(titre);
+}
+
+
+
+
+
+
+
+
+
+
+
+   //affiche()
+/*const galery = (list) => {
+    let galery = document.querySelector(".gallery");
+    list.map((elt)=> {
         let createimg = createelement()
     })
     createelement
@@ -22,12 +44,9 @@ const galery = (listwork) => {
     //creer les elements html dans la fonction
     //modifier tableau
     let tab_galery = new Set([]);
-    /*for (let i of images.children) {
+    for (let i of images.children) {
         //recuperer l'attribut class qui se trouve dans images
         const id_images = Number (i.getAttribute("class").split("-").pop())
         
-    }*/
-}
-
-
-   
+    }
+}*/
--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,5 +1,5 @@
 //envoyer une requête HTTP de type GET au service web(fetch: aller chercher)
-fetch ("http://localhost:5678/api/works")
+/*fetch ("http://localhost:5678/api/works")
 //récupérer le résultat de la requête au format json
 .then(function (Res) {
        return Res.json();  
@@ -11,4 +11,4 @@ fetch ("http://localhost:5678/api/works")
   .catch(function(err) {
     // Une erreur est survenue
     console.log(err)
-  })
+  })*/
