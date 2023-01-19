0bab422d033cd9c0e6468d1ff730368d3f2247f9
dh ch <chaalaadhif@gmail.com>
2023-01-19 22:36:01 +0100

    refaire edit.js

--- FrontEnd/js/component/edit.js
+++ FrontEnd/js/component/edit.js
@@ -4,47 +4,88 @@ import { afficheGalleries, affiche, affichecategory, } from "./galery.js";
 const update = () => {
     //alert("ok");
     //login et logout
+    
     afficheGalleries();
     document.getElementById("login").style.display = "none";
     document.getElementById("logout").style.display = "block";
     //creation des elements
     //black header
-    const modifier1 = document.querySelector(".modifier1");
-    const blackdiv = document.createElement("div");
-    blackdiv.classList.add("blackheader");
-    modifier1.insertAdjacentElement("afterbegin", blackdiv);
+    let body = document.getElementsByTagName('body');
+    const blackdiv = document.createElement('div');
+    blackdiv.classList.add('blackheader');
+    body[0].insertAdjacentElement("afterbegin", blackdiv);
     //creer les elements de blackheader(icone et bouton de l'entete)
     const divedit = document.createElement("div");
     divedit.classList.add("row");
+    divedit.classList.add("modifier");
+    divedit.setAttribute("href", "#modal");
     divedit.innerHTML = `<i class="fa-regular fa-pen-to-square "></i>
-    <span>Mode édition</span>`;
+    <span>modifier</span>`;
     const btn_edit = document.createElement("button");
     btn_edit.innerHTML = "publier les changements";
     blackdiv.appendChild(divedit);
     blackdiv.appendChild(btn_edit);
-    modifier1.insertAdjacentElement("afterbegin", blackdiv);
+    //modifier1.insertAdjacentElement("afterbegin", blackdiv);
 
     //creation  de modif pour photo architecte
-    const modifier2 = document.querySelector(".modifier2");
+    const figure = document.getElementsByTagName("figure");
     const modifier_photo = document.createElement("div");
-    modifier_photo.classList.add("toright");
+    modifier_photo.classList.add("modifier");
+    divedit.setAttribute("href", "#modal");
     modifier_photo.innerHTML = `<i class="fa-regular fa-pen-to-square "></i>
     <span>modifier</span>`;
-    modifier2.insertAdjacentElement("beforeend", modifier_photo);
+    figure[0].insertAdjacentElement("beforeend", modifier_photo);
     //creation  de modif pour texte associée de la photo
-    const modifier3 = document.querySelector(".modifier3");
+    const article = document.getElementsByTagName("article");
     const modifier_text = document.createElement("div");
     modifier_text.classList.add("modifier");
+    divedit.setAttribute("href", "#modal");
     modifier_text.innerHTML = `<i class="fa-regular fa-pen-to-square "></i>
     <span>modifier</span>`;
-    modifier3.insertAdjacentElement("beforebegin", modifier_text);
+    article[0].insertAdjacentElement("afterbegin", modifier_text);
     //creation  de modif pour images de projet
-    const modifier4 = document.querySelector(".modifier4");
+    const portfolio = document.getElementById(".portfolio");
     const modifier_project = document.createElement("div");
     modifier_project.classList.add("modifier");
     modifier_project.innerHTML = `<i class="fa-regular fa-pen-to-square "></i>
     <span>modifier</span>`;
-    modifier4.appendChild(modifier_project);
+    portfolio.appendChild(modifier_project);
+    divedit.setAttribute("href", "#modal");
+
+   //ouvrir modal au clic
+   
+   const openmodal = function(e){
+    e.preventDefault();
+    const gettarget = ()=>{
+        const target= document.queryselector(e.target.getAttribute("href"))
+        target.style.display = null;
+        target.removeAttribute('aria-hidden');
+        }
+        const all =document.querySelectorAll(".modal");
+        
+
+    }
+
+    /*const clic_edit = document.querySelector(".modifier");
+    for()
+    clic_edit.addEventListener("click", () => {
+        openmodal();
+
+    })
+    const close_modal = document.querySelector(".close");
+    close_modal.addEventListener("click", ()=>{
+        document.getElementById("modal").style.display = "none";
+    })
+     const openmodal = function(e){
+        e.preventDefault();
+        //document.getElementById("modal").style.display = "block";
+        console.log('ok');
+        document.getElementById("modal").style.display = "null;
+    }
+    const modifier = document.getElementsByClassName(".modifier");
+    document.querySelectorAll(".modal").forEach(modifier =>{
+        modifier.addEventListener('click',openmodal)
+    })*/
 
 
 
